67272320907f56d9dbf75ab574c212cbe36956a9
undefined <undefined>
2022-03-13 22:28:03 +0800

    去除无用的依赖,报错

--- src/index.ts
+++ src/index.ts
@@ -2,13 +2,12 @@
  * @Author: JiaoZhe
  * @Date: 2022-03-12 14:23:38
  * @LastEditors: Please set LastEditors
- * @LastEditTime: 2022-03-13 21:42:17
+ * @LastEditTime: 2022-03-13 22:27:36
  * @FilePath: /notify-server/src/index.ts
  * @Description: 这个文件做了什么
  */
 import dotenv from 'dotenv'
 import LoveMsg from './libs/LoveMsg'
-import schedule from 'node-schedule'
 dotenv.config()
 
 // function init() {

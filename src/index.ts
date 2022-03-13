fcb4afb85b888a7a75590257a2360f4e7a6cc1c8
undefined <undefined>
2022-03-13 19:09:19 +0800

    添加定时任务

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,22 @@
+/*
+ * @Author: JiaoZhe
+ * @Date: 2022-03-12 14:23:38
+ * @LastEditors: Please set LastEditors
+ * @LastEditTime: 2022-03-13 19:02:11
+ * @FilePath: /notify-server/src/index.ts
+ * @Description: 这个文件做了什么
+ */
 import dotenv from 'dotenv'
 import LoveMsg from './libs/LoveMsg'
+import schedule from 'node-schedule'
+console.log('schedule', schedule)
 dotenv.config()
 
+function init() {
+  schedule.scheduleJob('30 10 * * * *', () => {
+    LoveMsg()
+  })
+}
+init()
 // 早安、午安、晚安 => 由环境变量控制
-LoveMsg()
+// LoveMsg()
--- src/libs/LoveMsg/templates/text.ts
+++ src/libs/LoveMsg/templates/text.ts
@@ -1,8 +1,8 @@
 /*
  * @Author: JiaoZhe
  * @Date: 2022-03-12 14:23:38
- * @LastEditors:
- * @LastEditTime: 2022-03-12 15:34:33
+ * @LastEditors: Please set LastEditors
+ * @LastEditTime: 2022-03-12 21:00:36
  * @FilePath: /notify-server/src/libs/LoveMsg/templates/text.ts
  * @Description: 这个文件做了什么
  */
@@ -16,17 +16,17 @@ import dayjs, { weekToday } from '../../../utils/dayjs'
 export const textTemplate = (data: TextTemplateProps) => {
   const { caiHongpi, sayLove, songLyrics, oneMagazines, netEaseCloud, oneWord, dayEnglish } = data
 
-  let text = '早安呀，我可爱的xxx~\n'
+  let text = '早安呀，我可爱的小七公主👸🏻~\n'
 
   // 工作日/休息日，需要排除节假日
   const week = weekToday()
   if (['星期六', '星期日'].includes(week)) {
     text += `
-如果我xxx已经起床啦！崽崽向你说xx呦~，记得吃早饭呀😆\n
+如果我家小七已经起床啦！崽崽向你说xx呦~，记得吃早饭呀😆\n
 嗯哼哼~今天可是${week}哦，上班别迟到了哦~`
   } else {
     text += `
-如果我xxx还没起床呀！崽崽就等着xx起床给我说xx呦🤣
+如果我家小七还没起床呀！崽崽就等着xx起床给我说xx呦🤣
 嗯哼~，既然今天是${week}，就让你再睡会懒觉~下次可不能啦~😝\n`
   }
 
